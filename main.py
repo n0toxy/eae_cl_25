54fa36a7c40d5a82c08b3bfa4a42b132ebc38ea0
Mickael LAURENT <mickael.laurent@ecole-air.fr>
2025-10-28 11:33:14 +0100

    majs

---
+++ main.py
@@ -0,0 +1,23 @@
+def main():
+    print("Hello from eae-cl-25!")
+
+class MaClasse:
+    def __init__(self, nom, age):
+        self.nom = nom  # on stocke un attribut propre à chaque objet
+        self.age = age  # on stocke un attribut propre à chaque objet
+        self._parametretest = 1.2
+
+    def parametretest(self):
+        return self._parametretest
+    
+    def direbonjour(self):
+        return f"Bonjour, je suis {self.nom} et j'ai {self.age*self.parametretest()} ans!"
+
+
+
+if __name__ == "__main__":
+    main()
+    a = MaClasse("Alice",25)
+    b = MaClasse("Bob",58)
+    print(a.direbonjour())
+    print(b.direbonjour())
